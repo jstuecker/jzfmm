3a682153c94500ff5ddea15c87d7a8a3cc6dd88a
Jens Stücker <jens.stuecker@univie.ac.at>
2025-09-11 22:39:27 +0200

    Updated Ilist knn benchmark

--- src/benchmarks/benchmark_knn_ilist.py
+++ src/benchmarks/benchmark_knn_ilist.py
@@ -1,6 +1,5 @@
 import jax
 import jax.numpy as jnp
-import fmdj
 import custom_jax as cj
 import matplotlib.pyplot as plt
 from scipy.spatial import cKDTree
@@ -8,34 +7,43 @@ from fmdj.utility import Tee, Timer
 import sys
 import numpy as np
 
-boxsize = 1.
+boxsize = 0.
 k = 16
 N = 512*1024
 
 sys.stdout = Tee(sys.stdout, open("logs/knn_ilist.log", "a+"))
 
-print(f"============== Ilist knn tests ==============")
+print(f"============== Lowest level Ilist build (alloc 300) ==============")
 timer = Timer(verbose=True, loops=50, print_compile=False, print_warmup=False)
 timer.set_tag(N=N, k=k)
 
 pos0 = jax.random.uniform(jax.random.PRNGKey(0), (N,3), minval=0, maxval=1, dtype=jnp.float32)
+posz, idz = cj.tree.pos_zorder_sort.jit(pos0)
 
-octree, posz, massz, isort_z = fmdj.fmm.build_octree_with_multipoles.jit(
-    pos0, jnp.ones(pos0.shape[0]), use_cj=True, max_leaf_size=32, p=1)
 
-res = cj.knn.brute_force_node_ilist_prep(octree, k=16)
-leaf_cent, level_leaf = res[0], res[1]
+spl, nleaf, llvl, xleaf, numleaves = cj.tree.summarize_leaves.jit(posz, max_size=32)
+spl2, nleaf2, llvl2, xleaf2, numleaves2 = cj.tree.summarize_leaves.jit(
+    xleaf, max_size=32*8, nleaf=nleaf, num_part=len(posz))
+il, ispl = cj.knn.build_ilist_recursive.jit(
+    xleaf2, llvl2, nleaf2, max_size=32*64, refine_fac=8, num_part=len(posz), k=16, boxsize=boxsize)
 
-radii, il, ilr, ispl = cj.knn.build_ilist_knn.jit(
-    *res, alloc_fac=180, k=k, sort=True, boxsize=boxsize, sort_alloc_fac=2.2)
+par = (xleaf, llvl, nleaf, spl2, il, ispl)
 
-rnn, inn = cj.knn.ilist_knn_search.jit(posz, octree.leaf_particle_bounds, leaf_cent, level_leaf, 
-                                       il, ispl, k=k, boxsize=boxsize)
+radii, il, ispl = cj.knn.build_ilist_knn.jit(*par, alloc_fac=180, k=k, sort=False, boxsize=boxsize)
+
+rnn, inn = cj.knn.ilist_knn_search.jit(posz, spl, xleaf, llvl, il, ispl, k=k, boxsize=boxsize)
 
 tree = cKDTree(posz, boxsize=boxsize if boxsize > 0 else None)
 rknn2, iknn2 = tree.query(posz, k=k)
 print(jnp.allclose(rnn, rknn2))
 print(jnp.allclose(inn, iknn2), jnp.sum(inn != iknn2))
 
-timer.timeit_jit(cj.knn.build_ilist_knn.jit, *res, alloc_fac=180, k=k, sort=True, name="sort")
-timer.timeit_jit(cj.knn.build_ilist_knn.jit, *res, alloc_fac=180, k=k, sort=False, name="nosort")
+for rfac in 2, 4, 8, 16:
+    timer.set_tag(rfac=rfac)
+    spl2, nleaf2, llvl2, xleaf2, numleaves2 = cj.tree.summarize_leaves.jit(
+        xleaf, max_size=32*rfac, nleaf=nleaf, num_part=len(posz))
+    il, ispl = cj.knn.build_ilist_recursive.jit(
+        xleaf2, llvl2, nleaf2, max_size=32*rfac*rfac, refine_fac=8, num_part=len(posz), k=16, boxsize=boxsize)
+    par = (xleaf, llvl, nleaf, spl2, il, ispl)
+    timer.timeit_jit(cj.knn.build_ilist_knn.jit, *par, alloc_fac=300, k=k, sort=False, name="nosort")
+    # timer.timeit_jit(cj.knn.build_ilist_knn.jit, *par, alloc_fac=180, k=k, sort=True, name="sort")
