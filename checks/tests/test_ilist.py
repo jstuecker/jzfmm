21a9110d884b754bf4714643b3f4db959258a204
Jens Stücker <jens.stuecker@univie.ac.at>
2025-10-19 23:36:12 +0200

    adapted ilist exception test

--- checks/tests/test_ilist.py
+++ checks/tests/test_ilist.py
@@ -1,12 +1,14 @@
 import fmdj
 import pytest
+import jax
 
 @pytest.mark.parametrize("ilist_fac", [512, 256, 128])
-def test_ilist_size_err(tree, ilist_fac):
+def test_ilist_size_exception(tree, ilist_fac):
+    """Here we create a scenario where the interaction list is too small and check that an error is raised."""
     octree, posz, massz, isortz = tree
-    ilist, nilist = fmdj.fmm.build_interaction_list.jit(octree, ilist_fac=ilist_fac)
-    # err.throw()
-    
-    assert nilist <= len(ilist), f"nfilled {nilist} exceeds ilist size {len(ilist)} for ilist_fac={ilist_fac}"
-    
-    
+    try:
+        ilist, nilist = fmdj.fmm.build_interaction_list.jit(octree, ilist_fac=ilist_fac)
+        assert ilist_fac == 512, "Expected MemoryError for small ilist_fac not raised"
+        assert nilist <= len(ilist), f"nfilled {nilist} exceeds ilist size {len(ilist)} for ilist_fac={ilist_fac}"
+    except (MemoryError, jax.errors.JaxRuntimeError) as err:
+        assert ilist_fac in (128, 256), "Unexpected RuntimeError raised"
