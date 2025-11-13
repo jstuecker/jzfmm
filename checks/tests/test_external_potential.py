5956105cc9178532e5945383a068451366ecb3c7
Jens Stücker <jens.stuecker@univie.ac.at>
2025-11-13 14:07:25 +0100

    added a unit test for external potential

---
+++ checks/tests/test_external_potential.py
@@ -0,0 +1,27 @@
+import fmdj
+import matplotlib.pyplot as plt
+import numpy as np
+import jax.numpy as jnp
+import jax
+import aegis
+import custom_jax as cj
+import pytest
+
+def test_nfw_acc():
+    prof = aegis.profiles.NFWProfile(conc=10., r200c=10.)
+    cfg = fmdj.config.Config()
+
+    host = aegis.profiles.NFWProfile(conc=6., m200c=1e12)
+    cfg.external_potential = fmdj.potential.NFWPotential(host.rs, host.rhoc)
+
+    pos = jax.random.normal(jax.random.PRNGKey(0), (10,3)) * 200.
+
+    phi1 = cfg.external_potential.potential(pos, cfg=cfg)
+    phi2 = host.potential(jnp.linalg.norm(pos, axis=-1))
+
+    acc1 = cfg.external_potential.acceleration(pos, cfg=cfg)
+    r = jnp.linalg.norm(pos, axis=-1)
+    acc2 = host.accr(r)[:,None] * (pos / r[:,None])
+    
+    assert phi1 == pytest.approx(phi2, rel=1e-5)
+    assert acc1 == pytest.approx(acc2, rel=1e-5)
