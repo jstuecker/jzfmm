fixed gradients and added a test and a benchmark

src/fmdj/potential.py
@@ -1,7 +1,7 @@
 from .config import Config, PotentialField
 import jax.numpy as jnp
 import jax
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass(unsafe_hash=True)
 class NFWPotential(PotentialField):
@@ -15,3 +15,10 @@ class NFWPotential(PotentialField):
         u = jnp.linalg.norm(x, axis=-1) / self.rs
         # The -1 is to set phi(r->0) = 0. This is numerically beneficial
         return self.phic(G=cfg.G()) * (jnp.log(1. + u) / u - 1.)
+    
+@dataclass(unsafe_hash=True)
+class UniformAcceleration(PotentialField):
+    acc : tuple[float, float, float] = (0., 0., 0.)
+
+    def potential(self, x, t=0., cfg=None):
+        return - (self.acc[0] * x[:,0] + self.acc[1] * x[:,1] + self.acc[2] * x[:,2])

src/fmdj/time_integration.py
@@ -111,24 +111,24 @@ simulate.vjp = jax.custom_vjp(simulate, nondiff_argnames=("tend", "nsteps", "cfg
 
 def simulate_fwd(p: Particles, tend: float, nsteps: int, cfg: Config, tstart: float = 0.) -> Particles:
     pcom = jnp.mean(p.apos(), axis=0)
-    fmdj.log("Starting forwards pass, <pos> = ({:.2f},{:.2f},{:.2f})",
+    fmdj.log("Starting forward pass, <pos> = ({:.2f},{:.2f},{:.2f})",
              pcom[0], pcom[1], pcom[2], level=1, cfg=cfg)
 
     p = simulate(p, tend, nsteps, cfg, tstart)
 
     pcom = jnp.mean(p.apos(), axis=0)
-    fmdj.log("Finished forwards pass, <pos> = ({:.2f},{:.2f},{:.2f})",
+    fmdj.log("Finished forward pass, <pos> = ({:.2f},{:.2f},{:.2f})",
              pcom[0], pcom[1], pcom[2], level=1, cfg=cfg)
 
     return p, p
 
 def simulate_bwd(tend: float, nsteps: int, cfg: Config, tstart: float, p: Particles, gp: jnp.ndarray):
-    dt = (tstart - tend) / nsteps
+    dt = (tend - tstart) / nsteps
 
     def step(i, carry):
         p, t, gp = carry
 
-        p = fmdj.time_integration.timestep.jit(p, dt=dt, cfg=cfg, t=t)
+        p = fmdj.time_integration.timestep.jit(p, dt=-dt, cfg=cfg, t=t)
         _, vjp_fun = jax.vjp(lambda p: fmdj.time_integration.timestep.jit(p, dt=dt, cfg=cfg, t=t), p)
         gxp, = vjp_fun(gp)
         return p, t + dt, gxp
@@ -136,8 +136,8 @@ def simulate_bwd(tend: float, nsteps: int, cfg: Config, tstart: float, p: Partic
     p_prev, t_prev, gp_prev = jax.lax.fori_loop(0, nsteps, step, (p, tend, gp))
 
     pcom = jnp.mean(p_prev.apos(), axis=0)
-    fmdj.log("Backward pass finished... tdiff = {:.2e}, <pos> = ({:.2f},{:.2f},{:.2f})",
-             t_prev-tstart, pcom[0], pcom[1], pcom[2], level=1, cfg=cfg)
+    fmdj.log("Finished backward pass, <pos> = ({:.2f},{:.2f},{:.2f})",
+             pcom[0], pcom[1], pcom[2], level=1, cfg=cfg)
     
     return (gp_prev,)
 