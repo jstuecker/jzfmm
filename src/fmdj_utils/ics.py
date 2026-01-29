65d0d560be35b5873be1498278f3d674e85724cb
Jens Stücker <jens.stuecker@univie.ac.at>
2026-01-29 14:41:03 +0100

    added num_total attribute to particles

--- src/fmdj_utils/ics.py
+++ src/fmdj_utils/ics.py
@@ -3,6 +3,8 @@ import jax
 import jax.numpy as jnp
 import aegis
 from fmdj.data import PosMass, Particles
+from jztree.comm import get_rank_info
+from jztree.tree import pos_zorder_sort
 
 def pad_pytree(x, num, float_val=jnp.nan, int_val=0):
     def pad(xi):
@@ -16,11 +18,13 @@ def pad_pytree(x, num, float_val=jnp.nan, int_val=0):
     return jax.tree.map(pad, x)
 
 def gaussian_blob(N, scale=1.0, mass=1., seed=0, zsort=False, npad=0):
+    rank, ndev, axis_name = get_rank_info()
+
     pos = jax.random.normal(jax.random.PRNGKey(seed), (N,3), dtype=jnp.float32) * scale
     if zsort:
-        pos, isort = jztree.tree.pos_zorder_sort(pos)
+        pos, isort = pos_zorder_sort(pos)
     mass0 = jnp.ones(len(pos), dtype=pos.dtype) * (mass/N)
-    posmass = PosMass(pos, mass0)
+    posmass = PosMass(pos=pos, mass=mass0, num_total=ndev*N)
 
     if npad > 0:
         return pad_pytree(posmass, npad)
@@ -32,7 +36,7 @@ def hernquist(N, a=1., M=1., anisotropy=0., seed=None):
         np.random.seed(seed)
     prof = aegis.profiles.HernquistProfile(a=a, M=M, anisotropy=anisotropy)
     pos, vel, mass = prof.sample_particles(N, result="pos_vel_m", rpmin=1e-6*a, ramax=1e6*a)
-    return Particles(pos, mass, vel)
+    return Particles(pos=pos, mass=mass, vel=vel)
 
 def discodj_sim(res, zsort=False):
     from discodj_examples.simulations import disco_sim
@@ -41,5 +45,5 @@ def discodj_sim(res, zsort=False):
         pos = jztree.tree.pos_zorder_sort(pos)[0]
 
     mass = jnp.ones(len(pos), dtype=pos.dtype) / res**3
-    return PosMass(pos, mass)
+    return PosMass(pos=pos, mass=mass)
 discodj_sim.jit = jax.jit(discodj_sim, static_argnames=("res", "zsort"))
