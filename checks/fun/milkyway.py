ab3cb9c475ad160802457053dcf90e4d3ec77f43
Jens Stücker <jens.stuecker@univie.ac.at>
2026-02-12 13:27:32 +0100

    add a MilkyWay example

---
+++ checks/fun/milkyway.py
@@ -0,0 +1,117 @@
+import numpy as np
+import aegis
+import jax
+import jax.numpy as jnp
+import fmdj
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+from fmdj_utils.plots import time_in_years
+from matplotlib.animation import FuncAnimation
+from fmdj.external_potential import MilkyWayPotential
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--show", action="store_true", help="Visualize on the fly")
+parser.add_argument("--dm", action="store_true", help="Include DM halo in potential")
+args = parser.parse_args()
+
+# ------------------------------------------------------------------------------------------------ #
+#                                        Initial Conditions                                        #
+# ------------------------------------------------------------------------------------------------ #
+
+nfw = aegis.profiles.NFWProfile(5, 1e12)
+
+r = 10.**jax.random.uniform(jax.random.key(0), int(1e5), jnp.float32, 0., jnp.log10(20.))
+xy = aegis.numerics.sample.random_direction(len(r), ndim=2)
+pos = jnp.stack([xy[...,0], xy[...,1], jax.random.normal(jax.random.key(1), len(xy))*0.1], axis=-1)*r[:,None]
+
+pot = MilkyWayPotential()
+accr = jnp.linalg.norm(pot.acceleration(pos, cfg=fmdj.Config()), axis=-1)
+vcirc = jnp.sqrt(jnp.clip(accr * r, 0., None))
+vel = jnp.cross(pos, jnp.array((0.,0.,1.))) * (vcirc/r)[:,None]
+vel = vel + jax.random.normal(jax.random.key(2), vel.shape) * 10.
+
+# We choose effectively mass-less particles, let dynamics be driven by external potential
+part = fmdj.data.Particles(pos=jnp.array(pos), vel=jnp.array(vel), mass=jnp.ones_like(pos[...,0])*1e-1)
+
+# ------------------------------------------------------------------------------------------------ #
+#                                             Plotting                                             #
+# ------------------------------------------------------------------------------------------------ #
+
+def myplot(t: float, p: fmdj.data.Particles, previous=None, skip=10, dm=True):
+    if previous is None:
+        fig, ax = plt.subplots(1, 1, figsize=[6.5, 6])
+        ax = plt.gca()
+
+        # make figure and plot frame black and font white
+        fig.patch.set_facecolor("black")
+        ax.set_facecolor("black")
+        
+        ax.set_xlim(-20, 20)
+        ax.set_ylim(-20, 20)
+        ax.set_xlabel("x [kpc]", color="white")
+        ax.set_ylabel("y [kpc]", color="white")
+
+        ax.tick_params(colors="white", which="both")
+        for spine in ax.spines.values():
+            spine.set_edgecolor("white")
+
+        title = ax.text(0.5,0.95, f't={time_in_years(t)/1e6:.0f} Myr', transform=ax.transAxes, 
+                        ha="center", color="white")
+        
+        if dm:
+            xi = np.linspace(-20., 20., 100)
+            x,y = np.meshgrid(xi,xi)
+            rho = np.clip(nfw.density(np.sqrt(x**2+y**2)), 0, 1e8)
+            plt.pcolormesh(x, y, np.log10(rho), shading="auto", cmap="cubehelix", alpha=0.5)
+        
+        color = plt.get_cmap("afmhot")(0.5 + 0.5*np.random.uniform(size=len(p.pos)))[::skip] # 0.4 + 0.6 * 
+
+        s1 = ax.scatter(p.apos()[::skip,0], p.apos()[::skip,1], s=4, alpha=0.1, label=f't={t:.2f}', c = color)
+    else:
+        fig, ax, s1, title = previous
+
+        title.set_text(f't={time_in_years(t)/1e6:.0f} Myr')
+        
+        s1.set_offsets(jnp.array([p.apos()[::skip,0], p.apos()[::skip,1]]).T)
+    
+    return fig, ax, s1, title
+
+dm = args.dm
+
+previous = myplot(0, part, dm=dm)
+fig, ax, s1, title = previous
+fig.subplots_adjust(left=0.1, right=0.96, top=0.96, bottom=0.09)
+
+def update(t_and_p):
+    global previous
+    previous = myplot(*t_and_p, previous=previous, skip=10)
+    fig, ax, s1, title = previous
+    return [s1,title]
+
+# ------------------------------------------------------------------------------------------------ #
+#                                         Run and Visualize                                        #
+# ------------------------------------------------------------------------------------------------ #
+
+cfg = fmdj.Config(softening=1e-2)
+cfg.tree.alloc_fac_nodes = 2.0
+cfg.external_potential = MilkyWayPotential()
+if not dm:
+    cfg.external_potential.halo = None
+
+sim_iter = fmdj.time_integration.simulate_with_outputs(
+    part, tend=nfw.tcirc(20.)*0.5, nout=300, steps_per_output=20, cfg=cfg,
+)
+
+# # Use the generator as the frames iterable
+ani = FuncAnimation(fig, update, frames=sim_iter, blit=True, interval=40, repeat=True,
+                    cache_frame_data=False)
+
+if args.show:
+    plt.show()
+else:
+    plt.close()
+
+    ani.save(f"output/milkyway{'_dm' if dm else '_nodm'}.mp4", writer="ffmpeg", fps=30, dpi=400)
--- src/fmdj/external_potential.py
+++ src/fmdj/external_potential.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import List
+import numpy as np
 import jax
 import jax.numpy as jnp
 
@@ -16,18 +18,90 @@ class NFWPotential(PotentialField):
         u = jnp.linalg.norm(x, axis=-1) / self.rs
         # The -1 is to set phi(r->0) = 0. This is numerically beneficial
         return self.phic(G=cfg.G()) * (jnp.log(1. + u) / u - 1.)
-    
+
 @dataclass(unsafe_hash=True)
 class HernquistPotential(PotentialField):
     a: float = 1.0
-    M: float = 1.0
+    mass: float = 1.0
 
     def potential(self, x: jax.Array, t: float = 0., cfg: Config = None) -> jax.Array:
-        return -cfg.G() * self.M / (jnp.linalg.norm(x, axis=-1) + self.a)
-    
+        return -cfg.G() * self.mass / (jnp.linalg.norm(x, axis=-1) + self.a)
+
 @dataclass(unsafe_hash=True)
 class UniformAcceleration(PotentialField):
     acc : tuple[float, float, float] = (0., 0., 0.)
 
     def potential(self, x: jax.Array, t: float = 0., cfg: Config = None) -> jax.Array:
         return - (self.acc[0] * x[:,0] + self.acc[1] * x[:,1] + self.acc[2] * x[:,2])
+
+@dataclass(unsafe_hash=True)
+class MiyamotoNagaiPotential(PotentialField):
+    """Miyamoto Nagai (1975) potential (https://articles.adsabs.harvard.edu/pdf/1975PASJ...27..533M)
+    see also http://astro.utoronto.ca/~bovy/AST1420/notes/notebooks/07.-Flattened-Mass-Distributions.html
+    """
+
+    mass: float = 2e10
+    b: float = 300e-6
+    a: float = 3000e-6
+
+    def potential(self, x: jax.Array, t: float = 0., cfg: Config = None) -> jax.Array:
+        R = jnp.sqrt(x[...,0]**2 + x[...,1]**2)
+        z = x[...,2]
+
+        return - cfg.G()*self.mass / jnp.sqrt(R**2 + (jnp.sqrt(z**2 + self.b**2) + self.a)**2)
+
+@dataclass(unsafe_hash=True)
+class DiskPotential(PotentialField):
+    """Potential of a disk, approximated by 3 MN potentials as in arxiv:1502.00627"""
+    mass: float
+    scale_radius: float
+    height: float
+
+    def _mn_pars(self):
+        coeffs = [{}, {}, {}]
+        coeffs[0]["M_over_Md"] = -0.0090, 0.0640, -0.1653, 0.1164, 1.9487
+        coeffs[1]["M_over_Md"] =  0.0173,-0.0903,  0.0877, 0.2029,-1.3077
+        coeffs[2]["M_over_Md"] = -0.0051, 0.0287, -0.0361,-0.0544, 0.2242
+        coeffs[0]["a_over_Rd"] = -0.0358, 0.2610, -0.6987,-0.1193, 2.0074
+        coeffs[1]["a_over_Rd"] = -0.0830, 0.4992, -0.7967,-1.2966, 4.4441
+        coeffs[2]["a_over_Rd"] = -0.0247, 0.1718, -0.4124,-0.5944, 0.7333
+
+        pars = [{},{},{}]
+        for i in range(0,3):
+            x = self.height / np.clip(self.scale_radius, 1e-20, None)
+            xn = np.array(x)[..., np.newaxis]**np.array([4,3,2,1,0])
+            pars[i]["M"] = np.sum(xn*coeffs[i]["M_over_Md"], axis=-1) * self.mass
+            pars[i]["a"] = np.sum(xn*coeffs[i]["a_over_Rd"], axis=-1) * self.scale_radius
+        
+        return pars
+
+    def potential(self, x: jax.Array, t: float = 0., cfg: Config = None) -> jax.Array:
+        pars = self._mn_pars()
+
+        R = jnp.sqrt(x[...,0]**2 + x[...,1]**2)
+        z = x[...,2]
+
+        pot = 0.
+        for p in pars:
+            pot = pot - cfg.G()*p["M"] / jnp.sqrt(R**2 + (jnp.sqrt(z**2 + self.height**2) + p["a"])**2)
+
+        return pot
+
+@dataclass(unsafe_hash=True)
+class MilkyWayPotential(PotentialField):
+    halo: PotentialField | None = NFWPotential(24.17, 4096193.) # <=> conc=8.71, M200c=1e12
+    bulge: PotentialField | None = HernquistPotential(a=5e2, mass=9e9)
+    star_disk: PotentialField | None = DiskPotential(mass=4.1e10, scale_radius=2.5e3, height=3.5e2)
+    gas_disk: PotentialField | None = DiskPotential(mass=1.9e10, scale_radius=7e3, height=8e1)
+
+    def potential(self, x, t=0, cfg=None):
+        val = 0.
+        if self.halo is not None:
+            val = val + self.halo.potential(x, t, cfg)
+        if self.bulge is not None:
+            val = val + self.bulge.potential(x, t, cfg)
+        if self.star_disk is not None:
+            val = val + self.star_disk.potential(x, t, cfg)
+        if self.gas_disk is not None:
+            val = val + self.gas_disk.potential(x, t, cfg)
+        return val
