38b533a68cbc5c1e038eb77d409d0ed910d7e800
Jens Stücker <jens.stuecker@univie.ac.at>
2026-02-15 22:52:02 +0100

    removed TreePlane from interaction evaluation

--- src/fmdj/fmm.py
+++ src/fmdj/fmm.py
@@ -3,8 +3,10 @@ from functools import partial
 import numpy as np
 import jax
 import jax.numpy as jnp
+from dataclasses import dataclass, field
 
-from jztree.data import TreePlane, PosMass, InteractionList, get_pos_mass, TreeHierarchy
+
+from jztree.data import TreePlane, PosMass, InteractionList, get_pos_mass, TreeHierarchy, PosLvl
 from jztree.tree import pos_zorder_sort, build_tree_hierarchy, grouped_dense_interaction_list
 
 from .config import Config
@@ -24,44 +26,44 @@ jax.ffi.register_ffi_target("BwdForceAndPotential", ffi_forces.BwdForceAndPotent
 #                                          M2L Evaluation                                          #
 # ------------------------------------------------------------------------------------------------ #
 
+@jax.tree_util.register_dataclass
+@dataclass(slots=True)
+class FMMChildData:
+    poslvl: PosLvl
+    mp: jax.Array
+
 def evaluate_plane_interactions(
         node_range: jax.Array,
-        spl_nodes: jax.Array,
-        plane: TreePlane,
-        mp: jax.Array,
-        ilist_lr: InteractionList | None = None,
-        cfg: Config = None
+        node_ilist: InteractionList,
+        spl: jax.Array,
+        child_data: FMMChildData,
+        cfg: Config
     ) -> Tuple[jax.Array, InteractionList]:
     """
     Evaluates M2L for a tree plane and generates child interaction list.
-    
-    Inputs:
-    - plane: TreePlane at current level
-    - plane_lr: TreePlane at lower resolution (parent level), optional
-    - ilist_lr: InteractionList at lower resolution, optional
-    - loc_lr: Local expansion at lower resolution, optional
-    - cfg: Configuration object
-    
+
     Outputs:
     - loc: multipole local expansion, shape (Nchild, M)
     - new_ilist: new interaction list for children
     """
-    ilist_alloc_size = cfg.fmm.ilist_alloc_fac * plane.size()
+    size = len(child_data.poslvl.pos)
+    ilist_alloc_size = cfg.fmm.ilist_alloc_fac * size
 
     assert ilist_alloc_size < 2**31, "So far only int32 supported {ilist_alloc_size/2**31}"
 
-    children = jnp.concatenate((plane.center(), plane.lvl.view(jnp.float32)[...,None]), axis=-1)
+    # children = jnp.concatenate((plane.center(), plane.lvl.view(jnp.float32)[...,None]), axis=-1)
+    children = child_data.poslvl.pos_lvl()
     
     # Determine output shapes
-    out_loc = jax.ShapeDtypeStruct(mp.shape, jnp.float32)
-    out_interaction_count = jax.ShapeDtypeStruct((plane.size(),), jnp.int32)
+    out_loc = jax.ShapeDtypeStruct(child_data.mp.shape, jnp.float32)
+    out_interaction_count = jax.ShapeDtypeStruct((size,), jnp.int32)
     
     # Count opened interactions and evaluate M2L
     loc, interaction_counts = jax.ffi.ffi_call(
         "CountInteractionsAndM2L",
         (out_loc, out_interaction_count, )
     )(
-        node_range, spl_nodes, ilist_lr.ispl, ilist_lr.iother, children, mp,
+        node_range, spl, node_ilist.ispl, node_ilist.iother, children, child_data.mp,
         p=np.int32(cfg.fmm.p),
         softening=np.float32(cfg.softening),
         opening_angle=np.float32(cfg.fmm.opening_angle)
@@ -75,18 +77,12 @@ def evaluate_plane_interactions(
         "InsertInteractions",
         (out_child_ilist,)
     )(
-        node_range, spl_nodes, ilist_lr.ispl, ilist_lr.iother, children, ispl_child,
+        node_range, spl, node_ilist.ispl, node_ilist.iother, children, ispl_child,
         opening_angle=np.float32(cfg.fmm.opening_angle)
     )[0]
 
     # Create interaction list from outputs
     new_ilist = InteractionList(ispl=ispl_child, iother=child_ilist)
-
-    # Evaluate L2L part
-    # if loc_lr is not None:
-    #     loc = loc + shift_local_to_children(
-    #         plane_lr.ispl, loc_lr, plane_lr.center(), plane.center(), cfg=cfg
-    #     )
     
     return loc, new_ilist
 evaluate_plane_interactions.jit = jax.jit(evaluate_plane_interactions, static_argnames=['cfg'])
@@ -100,8 +96,12 @@ def evaluate_interaction_hierarchy(tps: List[TreePlane], th: TreeHierarchy, mph,
 
     for i in reversed(range(0, len(tps))):
         node_range = jnp.array([0, nnodes_sup], dtype=jnp.int32)
-        mp = mph.get(i, size=tps[i].size())
-        loc_ch, ilist = evaluate_plane_interactions(node_range, spl_nodes, tps[i], mp, ilist, cfg=cfg)
+        child_data = FMMChildData(
+            poslvl=PosLvl(pos=tps[i].center(), lvl=tps[i].lvl),
+            mp=mph.get(i, size=tps[i].size())
+        )
+
+        loc_ch, ilist = evaluate_plane_interactions(node_range, ilist, spl_nodes, child_data, cfg=cfg)
 
         if loc is not None:
             loc_ch = loc_ch + shift_local_to_children(
