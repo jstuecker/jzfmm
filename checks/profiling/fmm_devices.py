4b3040a611e3ad52bc14b22e63b10590e1b722a3
Jens Stücker <jens.stuecker@univie.ac.at>
2026-06-19 12:29:35 +0200

    profile distributions and multi-GPU

---
+++ checks/profiling/fmm_devices.py
@@ -0,0 +1,90 @@
+import os
+import numpy as np
+import io
+import matplotlib.pyplot as plt
+
+from pytest_jax_bench import JaxBench
+import jax
+import fmdj
+from jztree_utils import ics
+
+
+jax.distributed.initialize()
+
+def bench_ndev(ndevices):
+	jb = JaxBench(jit_rounds=10, jit_warmup=2)
+
+	ns = np.logspace(5, 8, 7)[::-1]
+
+	ndevs = 2**np.arange(0, 10)
+	ndevs = ndevs[ndevs <= ndevices][::-1]
+
+	res = dict(ndevs=ndevs, ns=ns)
+
+	for ndev in ndevs:
+		mesh = jax.make_mesh((ndev,), axis_names=("gpus",), axis_types=jax.sharding.AxisType.Explicit)
+		res[str(ndev)] = []
+		res["std" + str(ndev)] = []
+		for n in ns:
+			if jax.process_index() == 0:
+				print(ndev, n)
+
+			softening = 0.1 * (ndev * float(n)) ** (-1.0 / 3.0)
+
+			cfg_fmm = fmdj.FMMConfig(
+				kernel=fmdj.PlummerKernel(softening=softening),
+			)
+
+			if ndev > 1:
+				part = ics.uniform_particles.smap(mesh, jit=True)(int(n), npad=int(max(n*0.2, 1e5)))
+				f = fmdj.fmm.fast_multipole_method.smap(mesh, jit=True)
+				timing = jb.measure(
+					fn_jit=f, part=part, cfg_fmm=cfg_fmm, write=False, result="locz"
+				)[0]
+			else:
+				part = ics.uniform_particles(int(n))
+				timing = jb.measure(
+					fn_jit=fmdj.fmm.fast_multipole_method.jit, part=part, cfg_fmm=cfg_fmm,
+					write=False, result="locz"
+				)[0]
+
+			res[str(ndev)].append(timing.jit_mean_ms)
+			res["std" + str(ndev)].append(timing.jit_std_ms)
+
+	return res
+
+
+os.makedirs("out", exist_ok=True)
+
+ndevices = jax.device_count()
+# ndevices = 64
+fname = f"out/fmm_devices_{ndevices}.npz"
+
+# if not os.path.exists(fname):
+if ndevices == jax.device_count():
+	res = bench_ndev(ndevices)
+	if jax.process_index() == 0:
+		np.savez(fname, **res)
+else:
+	res = np.load(fname)
+
+
+if jax.process_index() == 0:
+	plt.figure(figsize=(5,3.5))
+	# for ndev in (64,32,16,8,4,2,1):
+	for ndev in (1,2,4,8,16,32,64):
+		if ndev not in res["ndevs"]:
+			continue
+		color = plt.get_cmap("viridis")(np.log2(ndev) / 6)
+		label = f"{ndev} GPU{'s' if ndev > 1 else ''}"
+		plt.loglog(res["ns"], res[str(ndev)], label=label, marker="o", color=color)
+		print(ndev, res[str(ndev)][0])
+	plt.legend(ncol = 2)
+	plt.xlabel("N per GPU")
+	plt.ylabel("Time [ms]")
+
+	nlin = np.logspace(6.5, 8.)
+	plt.loglog(nlin, nlin/2.0e5, ls="dashed", color="black")
+	plt.annotate("linear scaling", (1.5e7, 4.5e1))
+
+	plt.savefig("out/fmm_devices.pdf", bbox_inches="tight")
---
+++ checks/profiling/fmm_single_gpu.py
@@ -0,0 +1,100 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+import numpy as np
+import io
+import matplotlib.pyplot as plt
+
+from pytest_jax_bench import JaxBench
+import jax
+import jax.numpy as jnp
+import time
+
+
+def get_part(n, mode="uniform", seed=0):
+	from jztree_utils import ics
+	import fmdj
+	from jztree.data import PosMass
+	if mode == "grid":
+		ng = int(np.cbrt(n))
+		xi = jnp.arange(ng, dtype=jnp.float32) * (1.0 / ng)
+		pos = jnp.stack(jnp.meshgrid(xi, xi, xi, indexing="ij"), axis=-1).reshape(-1, 3)
+		return PosMass(pos=pos, mass=1.0 / len(pos), num=len(pos), num_total=len(pos))
+	elif mode == "uniform":
+		return ics.uniform_particles(int(n), seed=seed)
+	elif mode == "normal":
+		return ics.gaussian_particles(int(n), seed=seed)
+	elif mode == "hernquist":
+		return ics.hernquist_posmass(int(n), seed=seed, a=1.0, total_mass=1.0)
+	elif mode == "discodj":
+		res = int(np.cbrt(n))
+		return ics.discodj_particles.jit(res, boxsize=1.0 * res)
+	else:
+		raise ValueError(f"Unknown mode {mode}")
+
+
+def bench_distributions():
+	import fmdj
+	from jztree.data import PosMass
+
+	jb = JaxBench(jit_rounds=10, jit_warmup=1)
+
+	t0 = time.time()
+
+	ns = np.logspace(5, 8, 7)
+	ns = ((np.cbrt(ns) // 4) * 4) ** 3
+	res = dict(n=ns, discodj=[], grid=[], uniform=[], normal=[], hernquist=[])
+
+	for mode in "discodj", "grid", "uniform", "normal", "hernquist":
+		for n in ns:
+			print(mode, n, time.time()-t0)
+
+			part = get_part(n, mode=mode)
+
+			softening = 0.1 * float(n) ** (-1.0 / 3.0)
+			cfg_fmm = fmdj.FMMConfig(
+				kernel=fmdj.PlummerKernel(softening=softening),
+			)
+			cfg_fmm.tree.alloc_fac_nodes = 1.2
+			cfg_fmm.alloc_fac_ilist = 64.
+
+			timing, loc = jb.measure(
+				fn_jit=fmdj.fmm.fast_multipole_method.jit,
+				part=part,
+				cfg_fmm=cfg_fmm,
+				write=False,
+			)
+			res[mode].append(timing.jit_mean_ms)
+
+	return res
+
+
+os.makedirs("out", exist_ok=True)
+
+if os.path.exists("out/fmm_distributions.npz"):
+# if False:
+	res = np.load("out/fmm_distributions.npz")
+else:
+	res = bench_distributions()
+	np.savez("out/fmm_distributions.npz", **res)
+
+plt.figure(figsize=(5,3.5))
+plt.loglog(res["n"], res["grid"], label="grid", marker="o")
+plt.loglog(res["n"], res["uniform"], label="uniform", marker="o")
+plt.loglog(res["n"], res["normal"], label="normal", marker="o")
+plt.loglog(res["n"], res["discodj"], label="cosm. (no wrapping)", marker="o")
+plt.loglog(res["n"], res["hernquist"], label="hernquist", marker="o")
+
+# nlin = np.logspace(6.5, 8.)
+# plt.loglog(nlin, nlin / 3.5e5, ls="dashed", color="black")
+# plt.annotate("linear scaling", (1.2e7, 2.2e1))
+
+nlin = np.logspace(6.5, 8.)
+plt.loglog(nlin, nlin/2.0e5, ls="dashed", color="black")
+plt.annotate("linear scaling", (1.5e7, 4.5e1))
+
+plt.legend()
+plt.xlabel("N")
+plt.ylabel("Time [ms]")
+
+plt.savefig("out/fmm_distributions.pdf", bbox_inches="tight")
